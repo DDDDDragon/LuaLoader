f06a9bf73e28d061a99716343dbb66c969717058
DDDDDragon <2294632065@qq.com>
2023-03-20 22:01:31 +0800

    use termux

--- ItemLuaLoader.cs
+++ ItemLuaLoader.cs
@@ -3,17 +3,38 @@ using System.IO;
 using LiteDB;
 using Terraria;
 using Terraria.ModLoader;
+using System.Collections.Generic;
 
 namespace LuaLoader
 {
     public class ItemLuaLoader
     {
         private static string ItemsPath = ModLoader.ModPath + "\\LuaLoader\\Items";
+	public Dictionary<string, string> Lua;
         public void init()
         {
             if (!Directory.Exists(ItemsPath)) Directory.CreateDirectory(ItemsPath);
+		    lua = new Dictionary<string, string>();
             var db = new LiteDatabase($"{ItemsPath}\\Items.ldb");
-            var Items = db.GetCollection("Item");
+            var Items = db.GetCollection<LuaItem>();
+	        foreach(var item in Items)
+	        {
+		        if(item.name != "")
+		        {
+		             if(File.Exists($"{ItemsPath}\\{item.name}.lua"))
+				     {
+						 
+					 }
+
+		}
+	    }
         }
     }
+    public class LuaItem
+    {
+	public string name;
+	public string discription;
+    }
+
 }
+
