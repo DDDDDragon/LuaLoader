ff7a5afedf42beb6f2853561e2e4e182f815f433
DDDDDragon <72903970+DDDDDragon@users.noreply.github.com>
2023-04-04 23:01:07 +0800

    Create testItem.lua
    override测试

---
+++ testItem.lua
@@ -0,0 +1,15 @@
+function SetDefault_testItem()
+	item.damage = 50
+	item.width = 40
+	item.height = 40
+	item.useTime = 20
+	item.useAnimation = 20
+	item.useStyle = 1
+	item.knockBack = 6
+	item.value = 10000
+	item.rare = 2
+	item.autoReuse = true
+end
+function UpdateInventory_testItem()
+  NewText("hello override!")
+end
