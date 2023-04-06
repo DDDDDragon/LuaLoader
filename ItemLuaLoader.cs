59a4cb19f48ce69dd0f43714abada42daa903eca
DDDDDragon <72903970+DDDDDragon@users.noreply.github.com>
2023-04-06 22:59:24 +0800

    Create ItemLuaLoader.cs

--- ItemLuaLoader.cs
+++ ItemLuaLoader.cs
@@ -45,8 +45,8 @@ namespace LuaLoader
             //litem.overrideMethods.Add("UpdateInventory");
             var reg = "override function[ ]*([a-zA-Z]*)_"
             var reg2 = "override[ ]*nobase[ ]*function[ ]*([a-zA-Z]*)\\(";
-            foreach(var match in Regex.Matches(llua, reg)) litem.overrideMethods.Add(match.Groups[1], true);
-            foreach(var match in Regex.Matches(llua, reg2)) litem.overrideMethods.Add(match.Groups[1], false);
+            foreach(var match in Regex.Matches(llua, reg)) litem.overrideMethods.Add((match.Groups[1], true));
+            foreach(var match in Regex.Matches(llua, reg2)) litem.overrideMethods.Add((match.Groups[1], false));
 	    llua = Regex.Replace(llua, "override[ ]*", "");
             llua = Regex.Replace(llua, "nobase[ ]*", "");
             items.Add(litem);
